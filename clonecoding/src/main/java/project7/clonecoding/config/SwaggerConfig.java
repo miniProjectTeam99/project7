459a49002ae24ab5e2b7ca4abfc94116ab82a124
kjk0122 <juke9548@gmail.com>
2023-01-25 20:53:54 +0900

    Game 쪽 CRUD 틀 완성(제대로 구현은 진행중)+Swagger 완료 (User는 실험을 위해 만듬. 만드시는 분은 알아서 덮어버리면 됩니다.)

--- clonecoding/src/main/java/project7/clonecoding/config/SwaggerConfig.java
+++ clonecoding/src/main/java/project7/clonecoding/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
-    @Bean
+    @Bean //추가
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
