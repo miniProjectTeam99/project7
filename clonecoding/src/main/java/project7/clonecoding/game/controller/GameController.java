835df9cf07de627af037c97f6aa1131e26ce787d
anwonseok <anwonseok72@gmail.com>
2023-01-30 12:11:07 +0900

    ãgame 정보 조회 서비스 추가

--- clonecoding/src/main/java/project7/clonecoding/game/controller/GameController.java
+++ clonecoding/src/main/java/project7/clonecoding/game/controller/GameController.java
@@ -21,37 +21,18 @@ public class GameController {
 
     private final GameService gameService;
 
-    //게임 id로 단건 조회
-    @ApiOperation(value = "게시글 단일 상세 조회", notes = "게시글의 id로 목록을 상세 조회한다.")
-    @GetMapping("/games/{gameId}")
-    public GameResponseDto getGame(@PathVariable Long gameId) {
-        return gameService.getGame(gameId);
-    }
-
-    //게임 id로 전체 조회
-    @ApiOperation(value = "게시글 전체 조회", notes = "게시글의 메인 페이지 목록을 조회한다.")
+    //게임 목록 전체 조회
+    @ApiOperation(value = "게임 전체 조회", notes = "게임 목록의 전체정보를 조회한다.")
     @GetMapping("/games")
     public GameResponseDto getGame() {
         return gameService.getGames();
     }
 
-    //게시글 id로 게시글 수정하기
-    @ApiOperation(value = "게시글 수정", notes = "게시글의 id로 찾아 내용을 수정한다.")
-    @PutMapping("/games/{id}")
-    public ResponseDto updateGame(@PathVariable Long id, @RequestBody GameRequestDto request) {
-        return gameService.updateGame(id,request);
+    //게임 id로 단건 조회
+    @ApiOperation(value = "게임 단일 상세 조회", notes = "게임 id로 목록을 상세 조회한다.")
+    @GetMapping("/games/{gameId}")
+    public GameResponseDto getGame(@PathVariable Long gameId) {
+        return gameService.getGame(gameId);
     }
 
-    //게시글 id로 게시글 수정하기
-    @ApiOperation(value = "평점 매기기", notes = "게시글의 id로 찾아서 그것의 별점을 부여한다.")
-    @PatchMapping("/games/{id}")
-    public ResponseDto rateStar(@PathVariable Long id, @RequestBody StarRequestDto star) {
-        return gameService.rateStar(id,star);
-    }
-    //id로 게시물 삭제
-    @ApiOperation(value = "게시글 삭제", notes = "게시글을 게시글의 id로 찾아 삭제한다.")
-    @DeleteMapping("/games/{id}")
-    public ResponseDto deleteGame(@PathVariable Long id) {
-        return gameService.DeleteGame(id);
-    }
 }
--- clonecoding/src/main/java/project7/clonecoding/game/service/GameService.java
+++ clonecoding/src/main/java/project7/clonecoding/game/service/GameService.java
@@ -22,55 +22,16 @@ public class GameService {
 
     //게임 정보 단건으로 보내기 (추가한 내용)
     public GameResponseDto getGame(@PathVariable Long id) {
-
         Game game = gameRepository.findById(id).orElseThrow(
                 ()-> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
         );
-
         return new GameResponseDto(game);
     }
 
-    //게임 정보 단건으로 보내기 (추가한 내용)
+    //게임 정보 전체 조회
     public GameResponseDto getGames() {
-
         List<Game> game = gameRepository.findAll();
-
         return new GameResponseDto(game);
-    }
-
-    //게시물 수정
-    @Transactional
-    public ResponseDto updateGame(Long id, GameRequestDto request) {
-        //id로 게시물 찾기
-        Game game = findGameById(id);
-        //게시물 수정
-//        game.update(request);
-        return new ResponseDto("수정 완료.");
-    }
-    //게시물 수정
-    @Transactional
-    public ResponseDto rateStar(Long id, StarRequestDto request) {
-        //id로 게시물 찾기
-        Game game = findGameById(id);
-        //게시물 수정
-//        game.update(request);
-        return new ResponseDto("평점 매기기 완료.");
-    }
-    //게시물 delete
-    @Transactional
-    public ResponseDto DeleteGame(Long id) {
-        gameRepository.findById(id).orElseThrow(
-                ()-> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
-        );
-        //게시글 삭제
-        gameRepository.deleteById(id);
-        return new ResponseDto("삭제 완료.");
-    }
-    //게시물 단건 조회할 때 사용
-    public Game findGameById(Long id){  //살아있는 게시글만 찾기
-        return gameRepository.findById(id).orElseThrow(
-                ()-> new IllegalArgumentException("해당 게시물은 존재하지 않습니다.")
-        );
     }
 
 }
